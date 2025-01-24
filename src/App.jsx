3cd231fa840406df30783ba7d9bc10290373c6ad
Kawthar1512 <kawthar1512@yahoo.com>
2025-01-24 20:43:54 +0100

    i removed the useless  space

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import React from "react";
 import "./App.css";
-
 import NavBar from "./components/Navbar.jsx";
 import Section from "./components/Section.jsx";
 import Section2 from "./components/Section2.jsx";
@@ -11,20 +8,16 @@ import ImageGallery from "./components/Gallery.jsx";
 import Accordion from "./components/FAQ.jsx";
 import Footer from "./components/Footer.jsx";
 
-
 export default function App() {
   return (
     <>
-     <ScrollEffects />
+      <ScrollEffects />
       <NavBar />
       <Section />
       <Section2 />
       <ImageGallery />
       <Accordion />
       <Footer />
-
-
-     
     </>
   );
 }
